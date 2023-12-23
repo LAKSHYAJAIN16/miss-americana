0a05eea31dd6bcd786cd988aa2c32eade5fb049a
Lakshya Jain <minecraftpogtechno@gmail.com>
2023-12-23 16:59:10 -0500

    yes UI

--- ui/src/pages/Home.jsx
+++ ui/src/pages/Home.jsx
@@ -3,9 +3,18 @@ import Logo from "../assets/crown.png"; // Tell webpack this JS file uses this i
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center mt-[50px]">
-      <img src={Logo} className="h-32 w-32"/>
-      <h1 className="text-3xl font-bold text-pink-500">Hello world!</h1>
+    <div className="center flex flex-col justify-center items-center">
+      <img src={Logo} className="h-24 w-24" />
+      <h1 className="-mt-10 text-8xl font-bold text-pink-500 text-center font-dancing ">
+        miss americana
+      </h1>
+      <p className="text-lg text-white text-center font-dancing">
+        'because spotify sucks'
+      </p>
+
+      <button className="text-2xl bg-white mt-10 transition-all pl-10 pr-10 pt-3 pb-3 rounded-xl hover:scale-110 font-josepfin">
+        start vibing
+      </button>
     </div>
   );
 }
