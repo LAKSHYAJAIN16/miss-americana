634c71bdc01ec1f453de394dc9f40ef0d5ddabd1
Lakshya Jain <minecraftpogtechno@gmail.com>
2023-12-23 23:44:23 -0500

    bitch

--- ui/src/pages/Home.jsx
+++ ui/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Logo from "../assets/crown.png"; // Tell webpack this JS file uses this i
 export default function Home() {
   return (
     <div className="center flex flex-col justify-center items-center">
-      <img src={Logo} className="h-24 w-24" />
+      <img src={Logo} className="h-20 w-20" />
       <h1 className="-mt-10 text-8xl font-bold text-pink-500 text-center font-dancing ">
         miss americana
       </h1>
@@ -12,7 +12,7 @@ export default function Home() {
         'because spotify sucks'
       </p>
 
-      <button className="text-2xl bg-white mt-10 transition-all pl-10 pr-10 pt-3 pb-3 rounded-xl hover:scale-110 font-josepfin">
+      <button className="text-2xl bg-white mt-10 transition-all pl-9 pr-9 pt-1 pb-1 rounded-xl hover:scale-110 font-josepfin">
         start vibing
       </button>
     </div>
